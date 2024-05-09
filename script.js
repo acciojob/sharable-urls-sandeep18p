4433ae5ce3b461b4204d8cd6f6d83528e60585c8
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-05-09 14:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const h3 = document.getElementById("url");
+    h3.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+});
